b74acfe96a6ebf21d4de3aa7a5ebc3027810aa80
tlavette <tlavette@yahoo.com>
2019-06-12 23:06:25 -0400

    updated to correct app information.  scraper corrected.

--- server.js
+++ server.js
@@ -1,54 +1,152 @@
-
-// Dependency list
-var cheerio = require("cheerio");
-var axios = require("axios");
+// Dependencies
 var express = require("express");
-const path = require("path");
+var mongojs = require("mongojs");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
 
-// Set up Express App
+// Initialize Express
 var app = express();
-var PORT = process.env.PORT || 8080
 
-// Set up Express to handle data parsing.- standard
-app.use(express.urlencoded({ extended: true }));
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
 
-app.use(express.json());
-app.use(express.static("./public"));
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
 
+// Main route (simple Hello World Message)
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
 
-// Routes
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+// Retrieve data from the db
+app.get("/all", function(req, res) {
+  // Find all results from the scrapedData collection in the db
+  db.scrapedData.find({}, function(error, found) {
+    // Throw any errors to the console
+    if (error) {
+      console.log(error);
+    }
+    // If there are no errors, send the data to the browser as json
+    else {
+      res.json(found);
+    }
+  });
+});
 
+// Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
+  // Make a request via axios for the news section of `ycombinator`
+  axios.get("https://blackamericaweb.com/").then(function(response) {
+    // Load the html body from axios into cheerio
+    var $ = cheerio.load(response.data);
+    // For each element with a "title" class
+    $("div.post-title").each(function(i, element) {
+      // Save the text and href of each link enclosed in the current element
+      var title = $(element).children("a").text();
+      var link = $(element).parent("a").attr("href");
 
-// Making a request via axios for reddit's "webdev" board. We are sure to use old.reddit due to changes in HTML structure for the new reddit. The page's Response is passed as our promise argument.
-axios.get("https://blackamericaweb.com/").then(function(response) {
-
-  // Load the Response into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(response.data);
-
-  // An empty array to save the data that we'll scrape
-  var results = [];
-
-  // With cheerio, find each p-tag with the "title" class
-  // (i: iterator. element: the current element)
-  $("div.post-title").each(function(i, element) {
-
-    // Save the text of the element in a "title" variable
-    var title = $(element).text();
-
-    // In the currently selected element, look at its child elements (i.e., its a-tags),
-    // then save the values for any "href" attributes that the child elements may have
-    var link = $(element).parent().attr("href");
-
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title: title,
-      link: link
+      // If this found element had both a title and a link
+      if (title && link) {
+        // Insert the data in the scrapedData db
+        db.scrapedData.insert({
+          title: title,
+          link: link
+        },
+        function(err, inserted) {
+          if (err) {
+            // Log the error if one is encountered during the query
+            console.log(err);
+          }
+          else {
+            // Otherwise, log the inserted data
+            console.log(inserted);
+          }
+        });
+      }
     });
   });
 
-  // Log the results once you've looped through each of the elements found with cheerio
-  res.json(results);
+  // Send a "Scrape Complete" message to the browser
+  res.send("Scrape Complete");
 });
+
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Dependency list
+// var cheerio = require("cheerio");
+// var axios = require("axios");
+// var express = require("express");
+// const path = require("path");
+
+// // Set up Express App
+// var app = express();
+// var PORT = process.env.PORT || 8080
+
+// // Set up Express to handle data parsing.- standard
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(express.json());
+// app.use(express.static("./public"));
+
+
+// // Routes
+// // require("./routes/api-routes.js")(app);
+// require("./routes/html-routes.js")(app);
+
+
+// // Making a request via axios for reddit's "webdev" board. We are sure to use old.reddit due to changes in HTML structure for the new reddit. The page's Response is passed as our promise argument.
+// axios.get("https://blackamericaweb.com/").then(function(response) {
+
+//   // Load the Response into cheerio and save it to a variable
+//   // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+//   var $ = cheerio.load(response.data);
+
+//   // An empty array to save the data that we'll scrape
+//   var results = [];
+
+//   // With cheerio, find each p-tag with the "title" class
+//   // (i: iterator. element: the current element)
+//   $("div.post-title").each(function(i, element) {
+
+//     // Save the text of the element in a "title" variable
+//     var title = $(element).text();
+
+//     // In the currently selected element, look at its child elements (i.e., its a-tags),
+//     // then save the values for any "href" attributes that the child elements may have
+//     var link = $(element).parent().attr("href");
+
+//     // Save these results in an object that we'll push into the results array we defined earlier
+//     results.push({
+//       title: title,
+//       link: link
+//     });
+//   });
+
+//   // Log the results once you've looped through each of the elements found with cheerio
+//   res.json(results);
+// });
